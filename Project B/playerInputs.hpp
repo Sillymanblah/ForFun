460978cce11cac99ec25511f972020cd89a74a7a
Sillymanblah <54636916+Sillymanblah@users.noreply.github.com>
2022-03-18 20:59:46 -0500

    Update playerInputs.hpp

--- Project B/playerInputs.hpp
+++ Project B/playerInputs.hpp
@@ -1,7 +1,24 @@
 #include "boardUpdates.hpp"
 
 bool startGame() {
-  
+  char start;
+  cout << "Welcome to Battleship!" << endl;
+
+  for (;;) {
+    cout << "Would you like to play a game? (y/n)" << endl;
+    cin >> start;
+    
+    switch (start) {
+      case 'y':
+      case 'Y':
+        return 1;
+      case 'n':
+      case 'N':
+        return 0;
+      default:
+        cout << "Invalid selection, please select either 'y' or 'n'." << endl << endl;
+    }
+  }
 }
 
 void placement() {
