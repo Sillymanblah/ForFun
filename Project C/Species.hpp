20e44391484bcce545b5f6ec3c6f5706da0ee264
Sillymanblah <54636916+Sillymanblah@users.noreply.github.com>
2022-12-01 11:32:12 -0600

    Prep for Species class
    Simple file set-up for the class Species for Project C

---
+++ Project C/Species.hpp
@@ -0,0 +1,20 @@
+/*
+ - Author: John Grzegorczyk
+ - Date Started: 12/10/2022
+ - Title: Species Identification Class (Project C)
+ -
+ - Accompanying Code:
+ -   N/A
+*/
+
+#ifndef SPECIES_HPP
+#define SPECIES_HPP
+
+class Species {
+  private:
+
+  public:
+
+};
+
+#endif
